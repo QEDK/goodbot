95ed5c58509c3d3132e22f8af06b5a04b1fdd2e7
ankit <1994constant@gmail.com>
2020-03-31 01:04:00 +0530

    Fix breaking changes, linting

--- bot/bot.py
+++ bot/bot.py
@@ -26,12 +26,12 @@ class goodbot(object):
 		sender_full_name = msg["sender_full_name"]
 		message_type = msg["type"]  # "stream" or "private"
 		if message_type == "stream":
-			destination = msg["display_recipient"]  # destination is stream
+			destination = msg["stream_id"]  # destination is stream
 		else:
 			destination = sender_email  # destination is PM
 		topic = msg["subject"]  # topic legacy naming in API
 
-		print(content + "\nSucessfully heard.")
+		print(str(content) + "\nSuccessfully heard.")
 
 		if "just signed up for" in msg["content"]:  # hack for reading #announce stream
 			self.client.send_message({
@@ -72,13 +72,13 @@ class goodbot(object):
 							"to": destination,
 							"content": "Hello @**" + sender_full_name + "** ! You can ask me a question by adding the question after the command: `!help faq 'your question'`"
 						})
-					else:
-						self.client.send_message({
-							"type": message_type,
-							"topic": topic,
-							"to": destination,
-							"content": "Hello @**" + sender_full_name + "** ! In all our projects, we list the level of expertise and skills required. Although you can choose to share your skillset with organization administrators, all they might be able to do is point you to the list of projects you might have seen already on MediaWiki.org. It might be ideal if you assess for yourself which project best suits you based on the skills required, level of expertise needed and any topic area that interests you more. And, this would save everyone some time!"
-						})
+						return
+					self.client.send_message({
+						"type": message_type,
+						"topic": topic,
+						"to": destination,
+						"content": "Hello @**" + sender_full_name + "** ! In all our projects, we list the level of expertise and skills required. Although you can choose to share your skillset with organization administrators, all they might be able to do is point you to the list of projects you might have seen already on MediaWiki.org. It might be ideal if you assess for yourself which project best suits you based on the skills required, level of expertise needed and any topic area that interests you more. And, this would save everyone some time!"
+					})
 				if content[1].lower() == "wikipedia":
 					if(len(content) == 2):
 						self.client.send_message({
@@ -87,30 +87,30 @@ class goodbot(object):
 							"to": destination,
 							"content": "Hello @**" + sender_full_name + "** ! You can make me search Wikipedia by adding the query after the command: `!help wikipedia 'your query'`"
 						})
-					else:
-						self.client.send_message({
-							"type": message_type,
-							"topic": topic,
-							"to": destination,
-							"content": "Hello @**" + sender_full_name + "** ! This might take a while to process :time_ticking:"
-						})
-						query = ' '.join(content[2:])
-						self.client.send_message({
-							"type": message_type,
-							"topic": topic,
-							"to": destination,
-							"content": "Hey @**" + sender_full_name + "** Got it! :point_right: " + wikipedia.summary(query, sentences=2) + "\n"  # summary is a very slow call
-						})
-						response = ""
-						for result in wikipedia.search(query, results=1):
-							page = wikipedia.page(result)
-							response += ("* [" + page.title + "](" + page.url + ")\n")
-						self.client.send_message({
-							"type": "stream",
-							"topic": topic,
-							"to": destination,
-							"content": response
-						})
+						return
+					self.client.send_message({
+						"type": message_type,
+						"topic": topic,
+						"to": destination,
+						"content": "Hello @**" + sender_full_name + "** ! This might take a while to process :time_ticking:"
+					})
+					query = ' '.join(content[2:])
+					self.client.send_message({
+						"type": message_type,
+						"topic": topic,
+						"to": destination,
+						"content": "Hey @**" + sender_full_name + "** Got it! :point_right: " + wikipedia.summary(query, sentences=2) + "\n"  # summary is a very slow call
+					})
+					response = ""
+					for result in wikipedia.search(query, results=1):
+						page = wikipedia.page(result)
+						response += ("* [" + page.title + "](" + page.url + ")\n")
+					self.client.send_message({
+						"type": "stream",
+						"topic": topic,
+						"to": destination,
+						"content": response
+					})
 				if content[1].lower() == "stackoverflow":
 					if(len(content) == 2):
 						self.client.send_message({
@@ -119,59 +119,60 @@ class goodbot(object):
 							"to": destination,
 							"content": "Hello @**" + sender_full_name + "** ! You can make me search StackOverflow by adding the query after the command: `!help stackoverflow 'your query'`"
 						})
-					else:
-						self.client.send_message({
-							"type": message_type,
-							"topic": topic,
-							"to": destination,
-							"content": "Hello @**" + sender_full_name + "** ! This might take a while to process :time_ticking:"
-						})
-						query = ' '.join(content[2:])
-						stackoverflow = StackAPI('stackoverflow')
-						stackoverflow.page_size = 3  # lesser, the faster
-						stackoverflow.max_pages = 1  # will hit API only once
-						questions = stackoverflow.fetch('search/advanced', sort="relevance", q=query, order="desc", answers=1)
-						response = "\n**Closest match:** " + questions['items'][0]['title']
-						try:
-							answerjson = stackoverflow.fetch('answers/{ids}', ids=[str(questions['items'][0]['accepted_answer_id'])], filter="!9Z(-wzftf")  # filter code: default+"answer.body_markdown"
-							answer = "\n**Accepted answer:**\n" + answerjson['items'][0]['body_markdown']
-						except IndexError:  # faster than checking if index exists
-							answer = "\n**No accepted answer found**"
-						response += answer + "\nOther questions:\n"
-						response += '\n'.join(("* [" + question['title'] + "](" + question['link'] + ")") for question in questions['items'][1:])
-						self.client.send_message({
-							"type": "stream",
-							"subject": message_type,
-							"to": destination,
-							"content": "Hey @**" + sender_full_name + "** Got it! :point_down:\n" + response
-						})
+						return
+					self.client.send_message({
+						"type": message_type,
+						"topic": topic,
+						"to": destination,
+						"content": "Hello @**" + sender_full_name + "** ! This might take a while to process :time_ticking:"
+					})
+					query = ' '.join(content[2:])
+					stackoverflow = StackAPI('stackoverflow')
+					stackoverflow.page_size = 3  # lesser, the faster
+					stackoverflow.max_pages = 1  # will hit API only once
+					questions = stackoverflow.fetch('search/advanced', sort="relevance", q=query, order="desc", answers=1)
+					response = "\n**Closest match:** " + questions['items'][0]['title']
+					try:
+						answerjson = stackoverflow.fetch('answers/{ids}', ids=[str(questions['items'][0]['accepted_answer_id'])], filter="!9Z(-wzftf")  # filter code: default+"answer.body_markdown"
+						answer = "\n**Accepted answer:**\n" + answerjson['items'][0]['body_markdown']
+					except IndexError:  # faster than checking if index exists
+						answer = "\n**No accepted answer found**"
+					response += answer + "\nOther questions:\n"
+					response += '\n'.join(("* [" + question['title'] + "](" + question['link'] + ")") for question in questions['items'][1:])
+					self.client.send_message({
+						"type": message_type,
+						"to": destination,
+						"topic": topic,
+						"content": "Hey @**" + sender_full_name + "** Got it! :point_down:\n" + response
+					})
 				if content[1].lower() == "chat":
 					if(len(content) == 2):  # bunch all error control into one
 						self.client.send_message({
-							"type": "stream",
-							"subject": message_type,
+							"type": message_type,
 							"to": destination,
+							"topic": topic,
 							"content": "Hey @**" + sender_full_name + "** Please specify your required assistance in the following format:\n* `!help chat mediawiki` for assistance with MediaWiki software.\n* `!help chat wikimedia` for assistance with technical issues related to Wikimedia projects."
 						})
+						return
 					if(content[2].lower() == "wikimedia"):
 						self.client.send_message({
-							"type": "stream",
-							"subject": message_type,
+							"type": message_type,
 							"to": destination,
+							"topic": topic,
 							"content": "Hey @**" + sender_full_name + "** You can get help at the [#wikimedia-tech](https://webchat.freenode.net/?channels=wikimedia-tech) IRC channel. You can also request assistance on [Wikimedia Developer Support](https://discourse-mediawiki.wmflabs.org)"
 						})
 					if(content[2].lower() == "mediawiki"):
 						self.client.send_message({
-							"type": "stream",
-							"subject": message_type,
+							"type": message_type,
 							"to": destination,
+							"topic": topic,
 							"content": "Hey @**" + sender_full_name + "** You can get help at the [#mediawiki](https://webchat.freenode.net/?channels=mediawiki) IRC channel. You can also request assistance on [Wikimedia Developer Support](https://discourse-mediawiki.wmflabs.org) You can see a full list of available channels [here](https://meta.wikimedia.org/wiki/IRC/Channels#MediaWiki_and_technical)"
 						})
 		elif "goodbot" in content and content[0] != "!help":
 			self.client.send_message({
-				"type": "stream",
-				"subject": message_type,
+				"type": message_type,
 				"to": destination,
+				"topic": topic,
 				"content": "Hey there! :blush: What can I do for you today?"
 			})
 		else:
