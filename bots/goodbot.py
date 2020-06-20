146e578bdb1495eee3b564d006845da0d5c051d6
ankit <1994constant@gmail.com>
2020-06-21 00:51:46 +0530

    Modularize all greetings

--- bots/goodbot.py
+++ bots/goodbot.py
@@ -6,6 +6,7 @@ import configparser
 from rapidfuzz import fuzz
 from rapidfuzz import process as fuzzproc
 from pathlib import Path
+import random
 import json
 import re
 import os
@@ -24,6 +25,7 @@ class goodbot(object):
 			self.replies = json.load(file)
 		self.questions = list(question for question in self.faqs["questions"])
 		self.answers = self.faqs["answers"]
+		self.greetings = self.replies["greetings"].split(";")
 		self.subscribe_all()
 		print("Bot init complete")
 
@@ -60,6 +62,7 @@ class goodbot(object):
 		print(str(content) + "\nSuccessfully heard.")
 
 		try:
+			greeting = f"{random.choice(self.greetings)} @**{sender_full_name}**!"
 			if sender_email == "notification-bot@zulip.com" and topic == "signups":  # hack for reading #announce stream
 				username = re.search(r"\*\*.*\*\*", msg["content"])
 				if username is None:
@@ -70,7 +73,7 @@ class goodbot(object):
 					"type": message_type,
 					"topic": topic,
 					"to": destination,
-					"content": f"Hey {username}! Welcome to Wikimedia Zulipchat.\nIf you need any help with GSoD proposals, type `!help gsod`.\nIf you need any help with GSoC proposals, type `!help gsoc`.\nIf you need help with Outreachy proposals, type `!help outreachy`.\nYou can join the #**technical-support** channel to get technical help related to Wikimedia infrastructure.\nType `!help` for a full list of available commands. :blush:"
+					"content": f"{greeting} Welcome to Wikimedia Zulipchat.\nIf you need any help with GSoD proposals, type `!help gsod`.\nIf you need any help with GSoC proposals, type `!help gsoc`.\nIf you need help with Outreachy proposals, type `!help outreachy`.\nYou can join the #**technical-support** channel to get technical help related to Wikimedia infrastructure.\nType `!help` for a full list of available commands. :blush:"
 				})
 
 			if content[0].lower() == "!help" or content[0] == "@**goodbot**":
@@ -79,7 +82,7 @@ class goodbot(object):
 						"type": message_type,
 						"topic": topic,
 						"to": destination,
-						"content": f"Hey there, @**{sender_full_name}**! :blush: Here's what I can do for you:\nType `!help gsod` for help with GSoD proposals.\nType `!help gsoc` for help with GSoC proposals.\nType `!help outreachy` for help with Outreachy proposals.\nType `!help faq 'your question'` to search FAQs.\nType `!help wikipedia 'title'` to search articles.\nType `!help stackoverflow 'your question'` to search questions.\nType `!help chat mediawiki` to get help regarding MediaWiki software.\nType `!help chat wikimedia` to get technical help regarding Wikimedia. You can also join the #**technical-support** channel to get technical help related to Wikimedia infrastructure."
+						"content": f"{greeting} :blush: Here's what I can do for you:\nType `!help gsod` for help with GSoD proposals.\nType `!help gsoc` for help with GSoC proposals.\nType `!help outreachy` for help with Outreachy proposals.\nType `!help faq 'your question'` to search FAQs.\nType `!help wikipedia 'title'` to search articles.\nType `!help stackoverflow 'your question'` to search questions.\nType `!help chat mediawiki` to get help regarding MediaWiki software.\nType `!help chat wikimedia` to get technical help regarding Wikimedia. You can also join the #**technical-support** channel to get technical help related to Wikimedia infrastructure."
 					})
 				elif(len(content) > 1):
 					if content[1].lower() == "gsoc":
@@ -88,7 +91,7 @@ class goodbot(object):
 							"type": message_type,
 							"topic": topic,
 							"to": destination,
-							"content": f"Hello @**{sender_full_name}**! Here are some links to get you started.\nRead the information guide for GSoC participants: https://www.mediawiki.org/wiki/Google_Summer_of_Code/Participants\nRead the project ideas for this year: https://www.mediawiki.org/wiki/Google_Summer_of_Code/2020\nYou have been subscribed to the #**gsoc20-outreachy20** stream for further help."
+							"content": f"{greeting} Here are some links to get you started.\nRead the information guide for GSoC participants: https://www.mediawiki.org/wiki/Google_Summer_of_Code/Participants\nRead the project ideas for this year: https://www.mediawiki.org/wiki/Google_Summer_of_Code/2020\nYou have been subscribed to the #**gsoc20-outreachy20** stream for further help."
 						})
 					if content[1].lower() == "gsod":
 						self.subscribe_user("gsod20", sender_email)
@@ -96,7 +99,7 @@ class goodbot(object):
 							"type": message_type,
 							"topic": topic,
 							"to": destination,
-							"content": f"Hello @**{sender_full_name}**! Here are some links to get you started.\nRead the information guide for GSoD participants: https://www.mediawiki.org/wiki/Season_of_Docs/Participants\nRead the project ideas for this year: https://www.mediawiki.org/wiki/Season_of_Docs/2020\nYou have been subscribed to the #**gsod20** stream for further help."
+							"content": f"{greeting} Here are some links to get you started.\nRead the information guide for GSoD participants: https://www.mediawiki.org/wiki/Season_of_Docs/Participants\nRead the project ideas for this year: https://www.mediawiki.org/wiki/Season_of_Docs/2020\nYou have been subscribed to the #**gsod20** stream for further help."
 						})
 					if content[1].lower() == "outreachy":
 						self.subscribe_user("gsoc20-outreachy20", sender_email)
@@ -104,7 +107,7 @@ class goodbot(object):
 							"type": message_type,
 							"topic": topic,
 							"to": destination,
-							"content": f"Hello @**{sender_full_name}**! Here are some links to get you started.\nRead the information guide for Outreachy participants: https://www.mediawiki.org/wiki/Outreachy/Participants\nRead the project ideas for this year: https://www.mediawiki.org/wiki/Outreachy/Round_20\nYou have been subscribed to the #**gsoc20-outreachy20** stream for further help."
+							"content": f"{greeting} Here are some links to get you started.\nRead the information guide for Outreachy participants: https://www.mediawiki.org/wiki/Outreachy/Participants\nRead the project ideas for this year: https://www.mediawiki.org/wiki/Outreachy/Round_20\nYou have been subscribed to the #**gsoc20-outreachy20** stream for further help."
 						})
 					if content[1].lower() == "faq":
 						if(len(content) == 2):
@@ -112,7 +115,7 @@ class goodbot(object):
 								"type": message_type,
 								"topic": topic,
 								"to": destination,
-								"content": f"Hello @**{sender_full_name}**! You can ask me a question by adding the question after the command: `!help faq 'your question'`"
+								"content": f"{greeting} You can ask me a question by adding the question after the command: `!help faq 'your question'`"
 							})
 							return
 						lookup = self.fuzzymatch(" ".join(content[2:]))
@@ -123,7 +126,7 @@ class goodbot(object):
 								"type": message_type,
 								"topic": topic,
 								"to": destination,
-								"content": f"Hello @**{sender_full_name}**! {lookup}"
+								"content": f"{greeting} {lookup}"
 							})
 					if content[1].lower() == "wikipedia":
 						if(len(content) == 2):
@@ -131,21 +134,21 @@ class goodbot(object):
 								"type": message_type,
 								"topic": topic,
 								"to": destination,
-								"content": f"Hello @**{sender_full_name}**! You can make me search Wikipedia by adding the query after the command: `!help wikipedia 'your query'`"
+								"content": f"{greeting} You can make me search Wikipedia by adding the query after the command: `!help wikipedia 'your query'`"
 							})
 							return
 						self.client.send_message({
 							"type": message_type,
 							"topic": topic,
 							"to": destination,
-							"content": f"Hello @**{sender_full_name}**! This might take a while to process :time_ticking:"
+							"content": f"{greeting} This might take a while to process :time_ticking:"
 						})
 						query = " ".join(content[2:])
 						self.client.send_message({
 							"type": message_type,
 							"topic": topic,
 							"to": destination,
-							"content": f"Hey @**{sender_full_name}** Got it! :point_right: {wikipedia.summary(query, sentences=2)}"  # summary is a very slow call
+							"content": f"{greeting} Got it! :point_right: {wikipedia.summary(query, sentences=2)}"  # summary is a very slow call
 						})
 						response = ""
 						for result in wikipedia.search(query, results=1):
@@ -163,14 +166,14 @@ class goodbot(object):
 								"type": message_type,
 								"topic": topic,
 								"to": destination,
-								"content": f"Hello @**{sender_full_name}** ! You can make me search StackOverflow by adding the query after the command: `!help stackoverflow 'your query'`"
+								"content": f"{greeting} You can make me search StackOverflow by adding the query after the command: `!help stackoverflow 'your query'`"
 							})
 							return
 						self.client.send_message({
 							"type": message_type,
 							"topic": topic,
 							"to": destination,
-							"content": f"Hello @**{sender_full_name}**! This might take a while to process :time_ticking:"
+							"content": f"{greeting} This might take a while to process :time_ticking:"
 						})
 						query = ' '.join(content[2:])
 						stackoverflow = StackAPI('stackoverflow')
@@ -189,7 +192,7 @@ class goodbot(object):
 							"type": message_type,
 							"to": destination,
 							"topic": topic,
-							"content": f"Hey @**{sender_full_name}** Got it! :point_down:\n{response}"
+							"content": f"{greeting} Got it! :point_down:\n{response}"
 						})
 					if content[1].lower() == "chat":
 						if(len(content) == 2):  # TODO: bunch all error control into one
@@ -197,7 +200,7 @@ class goodbot(object):
 								"type": message_type,
 								"to": destination,
 								"topic": topic,
-								"content": f"Hey @**{sender_full_name}** Please specify your required assistance in the following format:\n* `!help chat mediawiki` for assistance with MediaWiki software.\n* `!help chat wikimedia` for assistance with technical issues related to Wikimedia projects."
+								"content": f"{greeting} Please specify your required assistance in the following format:\n* `!help chat mediawiki` for assistance with MediaWiki software.\n* `!help chat wikimedia` for assistance with technical issues related to Wikimedia projects."
 							})
 							return
 						if(content[2].lower() == "wikimedia"):
@@ -206,7 +209,7 @@ class goodbot(object):
 								"type": message_type,
 								"to": destination,
 								"topic": topic,
-								"content": f"Hey @**{sender_full_name}** You can get help at the [#wikimedia-tech](https://webchat.freenode.net/?channels=wikimedia-tech) IRC channel or at [Wikimedia Developer Support](https://discourse-mediawiki.wmflabs.org) on Discourse.\nYou have been subscribed to the #**technical-support** stream for further help."
+								"content": f"{greeting} You can get help at the [#wikimedia-tech](https://webchat.freenode.net/?channels=wikimedia-tech) IRC channel or at [Wikimedia Developer Support](https://discourse-mediawiki.wmflabs.org) on Discourse.\nYou have been subscribed to the #**technical-support** stream for further help."
 							})
 						if(content[2].lower() == "mediawiki"):
 							self.subscribe_user("technical-support", sender_email)
@@ -214,14 +217,14 @@ class goodbot(object):
 								"type": message_type,
 								"to": destination,
 								"topic": topic,
-								"content": f"Hey @**{sender_full_name}** You can get help at the [#mediawiki](https://webchat.freenode.net/?channels=mediawiki) IRC channel or at [Wikimedia Developer Support](https://discourse-mediawiki.wmflabs.org) on Discourse.\nYou can see a full list of available IRC channels [here](https://meta.wikimedia.org/wiki/IRC/Channels#MediaWiki_and_technical).\nYou have been subscribed to the #**technical-support** stream for further help."
+								"content": f"{greeting} You can get help at the [#mediawiki](https://webchat.freenode.net/?channels=mediawiki) IRC channel or at [Wikimedia Developer Support](https://discourse-mediawiki.wmflabs.org) on Discourse.\nYou can see a full list of available IRC channels [here](https://meta.wikimedia.org/wiki/IRC/Channels#MediaWiki_and_technical).\nYou have been subscribed to the #**technical-support** stream for further help."
 							})
 			elif "goodbot" in content and content[0] != "!help":
 				self.client.send_message({
 					"type": message_type,
 					"to": destination,
 					"topic": topic,
-					"content": "Hey there! :blush: What can I do for you today?"
+					"content": f"{greeting} :blush: What can I do for you today?"
 				})
 			else:
 				return
