869a83094ab364f6cd5222bec1cba96f1289f1c8
ankit <1994constant@gmail.com>
2020-07-10 00:00:21 +0530

    Fix linting, add better regex, dump file

--- bots/parsebot.py
+++ bots/parsebot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import requests
 import html2text
+import json
+from pathlib import Path
 import re
 
 r = requests.Session()
 projects = dict()
+
 i = 0
 while True:
 	i += 1
@@ -17,13 +20,16 @@ while True:
 		"disableeditsection": "true",
 		"disabletoc": "true",
 		"format": "json"
-		}
+	}
 	req = r.get(url="https://www.mediawiki.org/w/api.php", params=params)
 	try:
 		if req.json()["error"]:
 			break
 	except KeyError:
 		text = html2text.HTML2Text().handle(req.json()["parse"]["text"]["*"])
-		match = re.search(r"#?(?P<title>.*?\n)(?P<inner>.*)", text, flags=re.DOTALL)
+		match = re.search(r"#{1,}(?P<title>.*?\n)(?P<inner>.*)", text, flags=re.DOTALL)
 		projects[match.group("title").strip()] = match.group("inner").strip()
 		continue
+
+with open(Path(__file__).parents[1].joinpath("templates", "projects.json"), "w") as outfile:
+	json.dump(projects, outfile, indent="\t")
