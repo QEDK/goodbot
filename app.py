474e74e269ecdac6abd76410b51298eb22c2974b
QEDK <1994constant@gmail.com>
2020-08-07 17:40:45 +0530

    Fix lint, test status

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, Response
+import subprocess
 
 app = Flask(__name__)
 
+
 @app.route('/deploy', methods=['POST'])
 def respond():
-	print(request.json)
+	content = request.get_json(silent=True)
+	try:
+		if content["successful"]:
+			pass
+	except KeyError:
+		pass
 	return Response(status=200)
+
 
 @app.route('/')
 def index():
-	return "Hello world"
+	return subprocess.run("kubectl get pods")
