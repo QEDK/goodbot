4b082351d354d8d7557d5aa63986998dc8848afc
QEDK <1994constant@gmail.com>
2020-08-08 15:54:24 +0530

    Add Kubernetes deployment

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 from flask import Flask, Response, request, send_from_directory
 from flask_talisman import Talisman
+from kubernetes import client, config
+import yaml
 
 app = Flask(__name__)
 csp = {
 	"default-src": "'self'",
 	"style-src": "https://tools-static.wmflabs.org"
 }
-talisman = Talisman(app, content_security_policy=csp)
+talisman = Talisman(app, content_security_policy=csp, force_https=False)
+try:
+	config.load_kube_config()
+except Exception:  # to pass tests in non-Kubernetes context
+	pass
+apps_v1 = client.AppsV1Api()
 
 
 @app.route("/deploy", methods=["POST"])
 def respond():
 	content = request.get_json(silent=True)
 	try:
-		if content["successful"]:
-			pass
+		if content is not None and content["successful"]:
+			api_response = apps_v1.delete_namespaced_deployment(
+				name="goodbot.goodbot", namespace="default", body=client.V1DeleteOptions(propagation_policy="Foreground", grace_period_seconds=0))
+			print("Deployment deleted. status='%s'" % str(api_response.status))
+			with open("/data/project/ircpod.yaml") as f:
+				dep = yaml.safe_load(f)
+				resp = apps_v1.create_namespaced_deployment(body=dep, namespace="goodbot")
+				print("Deployment created. status='%s'" % resp.metadata.name)
+			api_response = apps_v1.delete_namespaced_deployment(
+				name="goodbot.ircbot", namespace="default", body=client.V1DeleteOptions(propagation_policy="Foreground", grace_period_seconds=0))
+			print("Deployment deleted. status='%s'" % str(api_response.status))
+			with open("/data/project/goodpod.yaml") as f:
+				dep = yaml.safe_load(f)
+				resp = apps_v1.create_namespaced_deployment(body=dep, namespace="goodbot")
+				print("Deployment created. status='%s'" % resp.metadata.name)
 	except KeyError:
 		pass
 	return Response(status=200)
@@ -23,3 +43,7 @@ def respond():
 @app.route("/")
 def index():
 	return send_from_directory("", "index.html")
+
+
+if __name__ == "__main__":
+	app.run(ssl_context="adhoc")
